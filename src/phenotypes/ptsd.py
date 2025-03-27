75e01eb4c26cae65733decb09f780f0d4c6d9b28
Roman.Nefedov <roman.nefedov@zi-mannheim.de>
2025-03-27 19:22:53 +0100

    commit

--- src/phenotypes/ptsd.py
+++ src/phenotypes/ptsd.py
@@ -5,19 +5,20 @@ from src.phenotypes import ICD10DerivedPhenoType, VerbalInterviewDerivedPhenoTyp
 from src.phenotypes.phenotype_names import PhenotypeName
 
 
+# The substraction of one is not necessary anymore
+# def pcl6_scorer(phenotype:ScoredBasedDerivedPhenoType)->Column:
+#
+#         pcl6_score = (
+#                 (phenotype.pcol(20497) - 1) +
+#                 (phenotype.pcol(20498) - 1) +
+#                 (phenotype.pcol(20495) - 1) +
+#                 (phenotype.pcol(20496) - 1) +
+#                 (phenotype.pcol(20494) - 1) +
+#                 (phenotype.pcol(20508) - 1))
+#         return pcl6_score
+#
 
-def pcl6_scorer(phenotype:ScoredBasedDerivedPhenoType)->Column:
-        pcl6_score = (
-                (phenotype.pcol(20497) - 1) +
-                (phenotype.pcol(20498) - 1) +
-                (phenotype.pcol(20495) - 1) +
-                (phenotype.pcol(20496) - 1) +
-                (phenotype.pcol(20494) - 1) +
-                (phenotype.pcol(20508) - 1))
-        return pcl6_score
-
-
-ptds_pcl6 =ScoredBasedDerivedPhenoType(name=PhenotypeName.PTSD_PCL6, phenotype_source_field_numbers=[20497, 20498, 20495, 20496, 20494, 20508], score_levels=[14],make_score_column=pcl6_scorer)
+ptds_pcl6 =ScoredBasedDerivedPhenoType(name=PhenotypeName.PTSD_PCL6, phenotype_source_field_numbers=[20497, 20498, 20495, 20496, 20494, 20508], score_levels=[14])
 
 
 
